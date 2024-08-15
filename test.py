0682e3caf6bebf91245d4e0e6c666fd450c4ce77
rebeckaw <rebecka.weegar@umu.se>
2024-08-15 09:00:57 +0200

    added failing test

--- test.py
+++ test.py
@@ -8,6 +8,11 @@ class TestAss1(unittest.TestCase):
     def hello(self):
         self.assertEqual(print_hello(), 'hello')
 
+class TestAss2(unittest.TestCase):
+    def test_2(self):
+        self.assertEqual(2, 1)
+
+
 
 
 if __name__ == '__main__':
